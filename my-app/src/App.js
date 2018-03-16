f456920c948294294f0f343542fe674cd7b7ad00
Jon Blake <jon.kollari@mail.utoronto.ca>
2018-03-16 12:56:05 -0400

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import Example from './components/classifierview'
+import './App.css';
+
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Example />
+
+      </div>
+      
+    );
+  }
+}
+
+export default App;

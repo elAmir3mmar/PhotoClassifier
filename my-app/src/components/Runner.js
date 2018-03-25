823c501a8d46b7ef405f69f47f7e44e669236e54
Ammar ElAmir <elAmir3mmar@users.noreply.github.com>
2018-03-25 01:05:15 -0400

    Attempting cattegory logging to log file.

--- my-app/src/components/Runner.js
+++ my-app/src/components/Runner.js
@@ -32,6 +32,7 @@ class Runner extends React.Component {
         <Button id='spacer' color="primary" size="lg" >J</Button>
         <Button id='spacer' color="primary" size="lg" >Space</Button>
         </p>
+        <div id='feedback'></div>
       </Jumbotron>
     </div>
   );
@@ -42,22 +43,89 @@ class Runner extends React.Component {
 handlePress(event) {
   // F is pressed
   if(event.keyCode === 70) {
-    alert('Call function to Record Category 1 and load next photo');
+    var category = 1;
+    var imageURL=urls[0];
+    logCategory(category, imageURL, fileName);
+    feedback("Assigned category: " + category + " to image: " + imageURL);
+    // alert('Call function to Record Category 1 and load next photo');
     }
     // J is pressed
   if(event.keyCode === 74) {
-    alert('Call function to Record Category 2 and load next photo');
+    var category = 2;
+    var imageURL=urls[0];
+    logCategory(category, imageURL, fileName);
+    // alert('Call function to Record Category 2 and load next photo');
   }
   if(event.keyCode === 32) {
-    alert('Call function to Record Category 3 and load next photo');
+    var category = 3;
+    var imageURL=urls[0];
+    logCategory(category, imageURL, fileName);
+    // alert('Call function to Record Category 3 and load next photo');
   }
 }}
+
+//Trying to update feedback
+function feedback(str) {
+  var container = document.getElementById("feedback");
+  var s = React.createClass({
+      render: function() {
+          return <p id="feedbackString">HERE</p>;
+      }
+  });
+  container.replaceChild(s);
+}
+
 
 // map images
 function importAll(r) {
   return r.keys().map(r);
 }
 
+//Should check if log file exists or create it.
+function initLog() {
+  var fs = require('fs');
+  var date = new Date();
+  var hour = date.getHours();
+  var minute = date.getMinutes();
+  var day = date.getDay();
+  var filename = "" + day + "_" + hour + ":" + minute + ".log";
+  
+  console.log(filename);
+
+      console.log(filename + ' does not exist');
+      var createFile = require('create-file');
+      createFile('../../public/logs/'+fileName, Date(), function (err) {
+        // file either already exists or is now created (including non existing directories)
+        if(err){
+          console.log("Failed to create file " + filename + " in ../../public/logs/");
+        } 
+      return filename;
+      });
+
+      console.log(filename + "already exists");
+      return filename;
+}
+
+//Function to write categories to file. 
+function logCategory (category, imageURL, fileName) {
+  var content = imageURL + "," + category
+  var writeFile = require('write');
+  writeFile('../../public/logs/'+fileName, content, function(err) {
+    if (err) console.log(err);
+  });
+  
+  // var fs = require('fs');
+  // const writeFileP = require("write-file-p");
+
+
+  // writeFileP('../../public/logs/'+fileName, content, (err, data) => {
+  //     console.log(err || data);
+  // });
+
+}
+
 // urls is now set to all images
 const urls = importAll(require.context('../../public/photos', false, /\.(png|jpe?g|svg)$/));
+// const fileName = initLog();
+const fileName = "log.log";
 export default Runner;
